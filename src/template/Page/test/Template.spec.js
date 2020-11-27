6a6adf348ec7a44ebb4895a81826415400c93b8c
zmc <z704600202@qq.com>
2020-11-27 17:53:08 +0800

    test✅ 修改

--- src/template/Page/test/Template.spec.js
+++ src/template/Page/test/Template.spec.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import Template from '../Template';
+// import React from 'react';
+// import renderer from 'react-test-renderer';
+// import Enzyme from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+// import Template from '../Template';
 
-Enzyme.configure({ adapter: new Adapter() });
+// Enzyme.configure({ adapter: new Adapter() });
 
 // describe('Template', () => {
 
