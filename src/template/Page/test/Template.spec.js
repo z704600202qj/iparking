cdedf2da727f40a4794149ad420ec4fb50dca6d5
zmc <z704600202@qq.com>
2020-11-27 17:50:10 +0800

    test✅ 修改测试文件

--- src/template/Page/test/Template.spec.js
+++ src/template/Page/test/Template.spec.js
@@ -2,10 +2,10 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import sdfs from '../sdfs';
+import Template from '../Template';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('sdfs', () => {
+describe('Template', () => {
 
 });
